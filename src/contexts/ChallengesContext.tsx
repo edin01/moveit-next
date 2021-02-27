1f11bea20c309c7f7c9c19d09714b5f538d5a1b3
Edson <edson.tavares1998@gmail.com>
2021-02-27 01:30:59 -0300

    Dia 4 - Finalização do sistema de level e exibição de notificações de novo desafio

--- src/contexts/ChallengesContext.tsx
+++ src/contexts/ChallengesContext.tsx
@@ -16,6 +16,7 @@ interface ChallengesContextData {
     levelUp: () => void;
     startNewChallenge: () => void;
     resetChallenge: () => void;
+    completeChallenge: () => void;
 }
 
 interface ChallengesProviderProps {
@@ -33,6 +34,10 @@ export function ChallengesProvider({ children }: ChallengesProviderProps) {
 
     const experienceToNextLevel = Math.pow((level + 1) * 4, 2)
 
+    useEffect(() => {
+        Notification.requestPermission()
+    }, [])
+
     function levelUp() {
         setLevel(level + 1)
     }
@@ -42,11 +47,39 @@ export function ChallengesProvider({ children }: ChallengesProviderProps) {
 
         const challenge = challenges[randomChallengeIndex]
 
+        new Audio('/notification.mp3').play()
+
+        if (Notification.permission === 'granted'){
+            new Notification('Novo desafio 🎉🎉',{
+                body: `Valendo ${challenge.amount}xp!`,
+                icon: `icons/${challenge.type}.svg`
+            })
+        }
+
         setActiveChallenge(challenge)
     }
 
     function resetChallenge() {
         setActiveChallenge(null)
+    }
+
+    function completeChallenge() {
+        if (!activeChallenge){
+            return;
+        }
+
+        const { amount } = activeChallenge;
+
+        let finalExperience = currentExperience + amount;
+
+        if (finalExperience >= experienceToNextLevel){
+            finalExperience = finalExperience - experienceToNextLevel
+            levelUp();
+        }
+
+        setCurrentExperience(finalExperience)
+        setActiveChallenge(null)
+        setChallengesCompleted(challengesCompleted + 1)
     }
 
     return (
@@ -59,7 +92,8 @@ export function ChallengesProvider({ children }: ChallengesProviderProps) {
                 experienceToNextLevel,
                 levelUp,
                 startNewChallenge,
-                resetChallenge
+                resetChallenge,
+                completeChallenge
             }}
         >
             {children}
